d99828910a32fc5e5104361691aa7255b9940da9
Kweayon  Clark <Kweayon@gmail.com>
2023-01-17 16:36:21 -0600

    Initial Commit

---
+++ src/constants/layout/Flex.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import GenStyledComponent from "../../types/styled_components/types";
+
+const Row: GenStyledComponent<"div", {}> = styled.div`
+
+    display: flex;
+
+`;
+
+const Column: GenStyledComponent<"div", {}> = styled.div`
+
+    display: flex;
+
+    flex-direction: column;
+
+`;
