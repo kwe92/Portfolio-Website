e5e96ab89765ce9622ea054a451499b7d46064b9
Kweayon  Clark <Kweayon@gmail.com>
2023-01-17 17:41:19 -0600

    Added ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { ThemeProvider } from "styled-components";
+import AppWrapper from "./AppStyles";
+
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
+import appTheme from "./theme/Theme";
+
+
 
 interface Props{
 
@@ -6,9 +15,23 @@ interface Props{
 
 const App = (props:Props) => {
     return (
-            <h1>
-                My App
-            </h1>
+      <ThemeProvider theme={appTheme}>
+
+         <AppWrapper>
+
+            <Navbar/>
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+
+          </AppWrapper>
+
+      </ThemeProvider>
+         
+          
     );
 };
 
