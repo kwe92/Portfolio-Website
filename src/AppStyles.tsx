3d6d9b26d3d4faaaf7587548df9127e93f37bfb9
Kweayon  Clark <Kweayon@gmail.com>
2023-01-18 17:28:12 -0600

    Update

--- src/AppStyles.tsx
+++ src/AppStyles.tsx
@@ -1,14 +1,10 @@
 import styled from "styled-components";
 import GenStyledComponent from "./types/styled_components/types";
 
-interface Props{
-    theme:{primaryColor:string}
-}
+const AppWrapper: GenStyledComponent<"div",{}> =
+styled.div`
 
-const AppWrapper: GenStyledComponent<"div",Props> =
-styled.div<Props>`
-
-    background: ${props => props.theme.primaryColor && props.theme.primaryColor}
+    background: ${({ theme }) => theme.primaryColor && theme.primaryColor}
 
 `;
 
