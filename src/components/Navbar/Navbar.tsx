241a2799be2651f07d208c4f28ee5c5c73424d88
Kweayon  Clark <Kweayon@gmail.com>
2023-01-18 17:26:14 -0600

    Update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { images } from "../../constants/images";
+import {ContainerNavbar, LogoImageContainer, LogoImage, NavLinkUL}from "./NavbarStyles";
 
-const Navbar = () => {
+interface Props {
+
+};
+
+
+const Navbar = (props:Props) => {
+
   return (
-    <div>Navbar</div>
+   <ContainerNavbar>
+
+      <LogoImageContainer>
+          <LogoImage src={images.logo2} alt="logo2"/>
+      </LogoImageContainer>
+
+      <NavLinkUL>
+        {navLinksItems}
+      </NavLinkUL>
+   </ContainerNavbar>
   );
-}
+};
+
+const linkText = ["home", "about", "work", "skills", "contact"];
+
+const navLinksItems = linkText.map((item, index) => 
+  <li key={index}>{item}</li>
+);
 
 export default Navbar;
