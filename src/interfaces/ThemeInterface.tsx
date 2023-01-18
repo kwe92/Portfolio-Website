d1ee9b198d0ebc47c59711efe3feda9f612b12fd
Kweayon  Clark <Kweayon@gmail.com>
2023-01-18 17:27:12 -0600

    Update

---
+++ src/interfaces/ThemeInterface.tsx
@@ -0,0 +1,19 @@
+export interface Theme {
+    
+    fontBase: string
+
+  , primaryColor: string
+
+  , secondaryColor: string
+
+  , blackColor: string
+
+  , lightgreyColor: string
+
+  , greyColor: string
+
+  , brownColor: string
+
+  , whiteColor: string
+
+}
