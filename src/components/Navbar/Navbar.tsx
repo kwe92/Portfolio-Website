d8730ce2148bd1160e9cee44e354b6e1d764983b
Kweayon  Clark <Kweayon@gmail.com>
2023-01-19 17:17:03 -0600

    Update Mobile Styles Added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../../constants/images";
 import {ContainerNavbar, LogoImageContainer, LogoImage, NavLinkUL, NavLinkListItem, NavLinkListItemWrapper, NavLinkListItemDot}from "./NavbarStyles";
+import { NavbarMobileMenu, HiMenu, Hi_X, FramerDiv, MobileNavLinkUL, MobileNavLinkListItem } from "./MobileLinksStyles";
+import { motion } from "framer-motion";
 
 interface Props {
 
@@ -8,6 +10,40 @@ interface Props {
 
 
 const Navbar = (props:Props) => {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handToggleOn = () => (setToggle(true));
+
+  const handToggleOff = () => (setToggle(false));
+
+  const handToggleBoth = () => (setToggle((prevState) => (!prevState)));
+
+
+  const linkText = ["home", "about", "work", "skills", "contact"];
+
+const navLinksItems = linkText.map((item) => 
+  <NavLinkListItemWrapper key={`link-${item}`}>
+
+    <NavLinkListItemDot/>
+    
+    <NavLinkListItem href={`#${item}`}>
+      {item}
+    </NavLinkListItem>
+
+  </NavLinkListItemWrapper>
+);
+
+const mobileNavLinksItems = linkText.map((item) => 
+
+  <NavLinkListItemWrapper key={item}>
+
+    <MobileNavLinkListItem href={`#${item}`} onClick={handToggleOff}>
+      {item}
+    </MobileNavLinkListItem>
+
+  </NavLinkListItemWrapper>
+);
 
   return (
    <ContainerNavbar>
@@ -19,17 +55,33 @@ const Navbar = (props:Props) => {
       <NavLinkUL>
         {navLinksItems}
       </NavLinkUL>
+
+      <NavbarMobileMenu>
+        <HiMenu onClick={handToggleBoth}/>
+      
+        {
+          toggle &&  
+          <MotionDiv>
+            <Hi_X onClick={handToggleOff}/>
+            <MobileNavLinkUL>
+            {mobileNavLinksItems}
+            </MobileNavLinkUL>
+          </MotionDiv>
+      }
+      </NavbarMobileMenu>
+
    </ContainerNavbar>
   );
 };
 
-const linkText = ["home", "about", "work", "skills", "contact"];
+const transition = { duration:0.85, ease: "easeOut" };
 
-const navLinksItems = linkText.map((item) => 
-  <NavLinkListItemWrapper key={`link-${item}`}>
-    <NavLinkListItemDot/>
-    <NavLinkListItem href={`#${item}`}>{item}</NavLinkListItem>
-  </NavLinkListItemWrapper>
-);
-
+const MotionDiv = (props:{ children: any }) => (
+    <FramerDiv
+    whileInView={{ x: [300, 0] }}
+    transition={transition}
+    >
+      {props.children} 
+    </FramerDiv>
+)
 export default Navbar;
