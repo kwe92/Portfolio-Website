bece851f8a8d43a72b49ce22226f87e8029015fd
Kweayon  Clark <Kweayon@gmail.com>
2023-01-21 17:01:12 -0600

    Update

--- src/container/About/About.tsx
+++ src/container/About/About.tsx
@@ -1,66 +1,56 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect, SetStateAction  } from "react";
 import { motion } from "framer-motion";
 import { AboutContainer, AboutHeadText, AboutProfilesContainer, AboutProfileContainer } from "./AboutStyles";
-import { images } from "../../constants/images/index"
-
-interface About {
-  title: string,
-   description: string,
-   imageUrl: string,
-};
+import { images } from "../../constants/images/index";
+import AboutInterface  from "../../interfaces/About";
+import useAbouts from "../../hooks/sanity_hooks/AboutsData";
+import { urlfor } from "../../client";
+import { SlowBuffer } from "buffer";
 
 const About = () => {
 
-  const abouts: About[] = [
-    {
-      title: "Software Development Principles",
-       description: "Software development is a passion of mine",
-       imageUrl: images.about01,
-    },
-    {
-      title: "Web Design",
-       description: "Good design should be kept in mind!",
-       imageUrl: images.about02,
-    },
-    {
-      title: "UI/UX",
-       description: "Simple clean responsive and adaptive",
-       imageUrl: images.about03,
-    },
-    {
-      title: "Web Developer",
-       description: "I love to create beautiful web applications that look good on any device.",
-       imageUrl: images.about04,
-    }
-  ];
+  // Custom Hook for API call
+  const abouts = useAbouts();
+
+  console.log("Abouts data: ", abouts);
 
   return (
     <AboutContainer>
-    <AboutHeadText>
-      i know that
-      <span> good design</span>
-      <br/>
-      means
-      <span> good business</span>
-    </AboutHeadText>
+      <AboutHeadText>
+        i know that
+        <span> good design</span>
+        <br/>
+        means
+        <span> good business</span>
+      </AboutHeadText>
 
-    <AboutProfilesContainer className="app__profiles">
-      {abouts.map((about, index) => (
-        <AboutProfileContainer key={`about-${index}`}
-        whileInView={{ opacity: 1 } }
-        whileHover={{ scale: 1.1}}
-        transition={{duration: 0.5, type: "tween "}}
-        className={"app__profile-item"}
-        >
-          <img src={about.imageUrl} alt={about.title} />
-          <h2 className="bold-text" style={{marginTop: "20px"}}>{about.title}</h2>
-          <p className="bold-text" style={{marginTop: "10px"}}>{about.description}</p>
+      <AboutProfilesContainer className="app__profiles">
+        {abouts.map((about, index) =>{
+          
+          const imageUrl = urlfor(about.imageUrl).url();
 
-        </AboutProfileContainer>
-      ))}
-    </AboutProfilesContainer>
+          return (
+              <AboutProfileContainer_ key={`about-${index}`}>
+
+                <img src={imageUrl} alt={about.title} />
+                <h2 className="bold-text" style={{marginTop: "20px"}}>{about.title}</h2>
+                <p className="bold-text" style={{marginTop: "10px"}}>{about.description}</p>
+                
+              </AboutProfileContainer_>
+          )
+        })}
+      </AboutProfilesContainer>
     </AboutContainer>
   );
 }
+
+const AboutProfileContainer_ = (props:{children: any, key: React.Key}) =>  <AboutProfileContainer key={props.key}
+whileInView={{ opacity: 1 } }
+whileHover={{ scale: 1.1}}
+transition={{duration: 0.5, type: "tween "}}
+className={"app__profile-item"}
+>
+  {props.children}
+</AboutProfileContainer>
 
 export default About;
