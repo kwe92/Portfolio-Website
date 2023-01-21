3a6834c0ac7d0cc7bab029f1d5d8cb605641e8a4
Kweayon  Clark <Kweayon@gmail.com>
2023-01-20 21:25:33 -0600

    Update

--- src/constants/images/Images.tsx
+++ src/constants/images/Images.tsx
@@ -33,6 +33,7 @@ import bolt from '../../assets/bolt.png';
 import nb from '../../assets/nb.png';
 import skype from '../../assets/skype.png';
 import spotify from '../../assets/spotify.png';
+import bgImg from '../../assets/bgIMG.png'; 
 
 export default {
   email,
@@ -70,4 +71,5 @@ export default {
   skype,
   spotify,
   mobileMenu,
+  bgImg,
 };
